d6e5cc4790b09e3d47cdcaa3bdd991c0447fe4ff
Joshua <joshuakruger66@live.com>
2024-08-22 15:25:03 +0200

    Updated the JavaScript and CSS to display a certificate for what we have achieved so far in this course.

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName'); 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
-      <h3>${studentName}</h3>
+      certificateContent. innerHTML= `
+        <h1>Certificate of Achievement</h2>
+        <p>This is to certify that</p>
+        <h1>${studentName}</h1>
+        <p>has almost completed the<p>
+        <h1>${courseName}!</h1>
+        <p>with legendary perseverance and world-class bad-assery for never giving up🏆</p>
+        <img src="logo.png" alt="Codespace's Logo" width="650px">
+        <p>${personalMessage}</p>
     `;
     
       //  Display the modal
@@ -39,8 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none'
     });
   });
   
